77274cfd121dafa60ef3e63e989259eed9861e9d
bigbit99 <bigbit831@gmail.com>
2023-02-20 03:15:27 +0900

    확인중

--- src/redux/modules/CommentsSlice.js
+++ src/redux/modules/CommentsSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-//instance
+//instance url
 const instance = axios.create({
   baseURL: process.env.REACT_APP_COMMENTS,
 });
