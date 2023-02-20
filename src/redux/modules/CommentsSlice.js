5e061c1d3d292aba4a3bdaf1c0a26db51607ac28
bigbit99 <bigbit831@gmail.com>
2023-02-20 22:15:23 +0900

    axios 수정

--- src/redux/modules/CommentsSlice.js
+++ src/redux/modules/CommentsSlice.js
@@ -12,7 +12,7 @@ export const __getComments = createAsyncThunk(
   'comments/getComments',
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.get('/api/comments');
+      const data = await axios.get('/comments');
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -25,7 +25,7 @@ export const __postComment = createAsyncThunk(
   'comments/postComments',
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.post('/api/comments', payload);
+      const data = await axios.post('/comments', payload);
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -38,7 +38,7 @@ export const __deleteComment = createAsyncThunk(
   'comments/deleteComments',
   async (payload, thunkAPI) => {
     try {
-      await axios.delete(`/api/comments/${payload}`);
+      await axios.delete(`/comments/${payload}`);
       return thunkAPI.fulfillWithValue(payload);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
@@ -51,7 +51,7 @@ export const __patchComment = createAsyncThunk(
   'comments/patcheComments',
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.patch(`/api/comments/${payload.id}`, payload);
+      const data = await axios.patch(`/comments/${payload.id}`, payload);
       return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
